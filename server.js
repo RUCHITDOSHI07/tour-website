f2dfe690af63a5faedfe63d8224653c48bd6577a
RUCHITDOSHI07 <ruchitdoshi37@gmail.com>
2024-05-02 00:18:30 +0530

    mongo changes

--- server.js
+++ server.js
@@ -13,7 +13,18 @@ app.use(bodyParser());
 
 app.use('/user',UserRouter)
 
-
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start your Express server
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.listen(port, () => {   
     console.log(`Server is running on http://localhost:${port}`);
